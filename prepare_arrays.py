2e79c8783d1e6d5a89f207b4605f69adb44689d4
JPacoch <pacochp@gmail.com>
2022-06-04 14:04:23 +0200

    windows from raster to png

--- prepare_arrays.py
+++ prepare_arrays.py
@@ -4,6 +4,8 @@ import rasterio.plot
 import matplotlib.pyplot as plt
 import os
 
+from PIL import Image
+
 points = gpd.read_file("data/wyzyny_train.gpkg")
 points['Y'] = points.centroid.x
 points['X'] = points.centroid.y
@@ -11,14 +13,13 @@ if not os.path.exists("arrays"):
     os.makedirs("arrays")
 with rio.open("data/Hillshade.tif") as hillshade:
     for x, y, index in zip(points.X, points.Y, range(0, points.shape[0])):
-        px, py = hillshade.index(x, y)
-        window = rio.windows.Window(px, py, 128, 128)
+        px, py = hillshade.index(y, x)
+        window = rio.windows.Window(py, px, 128, 128)
         clip = hillshade.read(1, window = window)
-        meta = hillshade.meta
-        meta['width'], meta['height'] = 128, 128
-        meta['transform'] = rio.windows.transform(window, hillshade.transform)
-        with rio.open(f"arrays/array{index}.tif", 'w', **meta) as dst:
-            dst.write(clip)
+        # plt.imshow(clip, cmap='binary')
+        # plt.show()
+        clip = Image.fromarray(clip)
+        clip.save(f"arrays/array{index}.png", format='PNG')
 
 
 print(points)

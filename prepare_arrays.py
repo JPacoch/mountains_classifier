968ca36c3aaca3512c6df5cbf2fe31afce6fdca5
Tomek Matuszek <tom.mateuszek@gmail.com>
2022-06-04 13:36:11 +0200

    wyczyszczona wersja

--- prepare_arrays.py
+++ prepare_arrays.py
@@ -7,18 +7,13 @@ import os
 points = gpd.read_file("data/wyzyny_train.gpkg")
 points['Y'] = points.centroid.x
 points['X'] = points.centroid.y
-os.makedirs("arrays")
+if not os.path.exists("arrays"):
+    os.makedirs("arrays")
 with rio.open("data/Hillshade.tif") as hillshade:
-    print(points.geometry.total_bounds)
-    print(hillshade.bounds)
-    #fig, ax = plt.subplots()
-    #rasterio.plot.show(hillshade.read(1), ax=ax)
-    #points.plot(ax=ax)
-    #plt.show()
     for x, y, index in zip(points.X, points.Y, range(0, points.shape[0])):
         px, py = hillshade.index(x, y)
         window = rio.windows.Window(px, py, 128, 128)
-        clip = hillshade.read(window = window)
+        clip = hillshade.read(1, window = window)
         meta = hillshade.meta
         meta['width'], meta['height'] = 128, 128
         meta['transform'] = rio.windows.transform(window, hillshade.transform)

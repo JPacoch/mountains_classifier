88366dd319f5c1a2a52ff54f8eb44f522c1d2c08
JPacoch <pacochp@gmail.com>
2022-06-04 14:10:21 +0200

    windows from raster to png

--- prepare_arrays.py
+++ prepare_arrays.py
@@ -14,7 +14,6 @@ if not os.path.exists("arrays"):
 
 with rio.open("data/Hillshade.tif") as hillshade:
     for x, y, index in zip(points.X, points.Y, range(0, points.shape[0])):
-<<<<<<< HEAD
         px, py = hillshade.index(y, x)
         window = rio.windows.Window(py, px, 128, 128)
         clip = hillshade.read(1, window = window)
@@ -22,15 +21,5 @@ with rio.open("data/Hillshade.tif") as hillshade:
         # plt.show()
         clip = Image.fromarray(clip)
         clip.save(f"arrays/array{index}.png", format='PNG')
-=======
-        px, py = hillshade.index(x, y)
-        window = rio.windows.Window(px, py, 128, 128)
-        clip = hillshade.read(window = window)
-        meta = hillshade.meta
-        meta['width'], meta['height'] = 128, 128
-        meta['transform'] = rio.windows.transform(window, hillshade.transform)
-        with rio.open(f"arrays/array{index}.tif", 'w', **meta) as dst:
-            dst.write(clip)
->>>>>>> be750e946e7c5ba5e68d019cdc29e9542dc7e34c
 
 print(points)
